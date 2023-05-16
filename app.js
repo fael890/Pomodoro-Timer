f97f5ad11ff9ea93f97b197bf96eb350469f7632
Rafael Marinho <rafael222.rc@gmail.com>
2023-05-16 17:02:40 -0300

    adicionando play e pause do timer

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+var timer;
+var seconds = 60;
+var minutes = 1;
+
+function changeTime(){
+    var format;
+
+    seconds--;
+
+    if(seconds == 0){
+        minutes--;
+        seconds = 60;
+    }
+
+    format = ( minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+    document.getElementById('timerNumbers').innerText = format;
+}
+
 function playTimer(){
+    timer = setInterval(()=>{
+        changeTime();
+    }, 50)
     
 }
 
