70077845cd56be28993e0f17b557352f10659857
Rafael Capodeferro <rafael222.rc@gmail.com>
2023-08-07 14:06:02 -0300

    Adding timer with work and break system

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # pomodoro-timer
-
-teste
--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-function playTimer(){
-    
+let workTime = 1;
+let breakTime = 1;
+let seconds = "00";
+let breakMoment = false;
+
+window.onload = () => {
+    document.getElementById("minutes").innerHTML = workTime;
+    document.getElementById("seconds").innerHTML = seconds;
 }
 
-function stopTimer(){
-    clearInterval(timer);
+function playTimer(){
+    seconds = 59;
+
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0;
+
+    let timerFunction = () => {
+        document.getElementById("minutes").innerHTML = workMinutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMinutes = workMinutes - 1;
+
+            if (workMinutes === -1) {
+                if(breakMoment === false){
+                    workMinutes = breakMinutes;
+                    breakMoment = true;
+                } else{
+                    workMinutes = workTime - 1;
+                    breakMoment = false;
+                    breakCount++;
+                }  
+            }
+
+            seconds = 59;
+        }
+    }
+
+    setInterval(timerFunction, 200);
 }
